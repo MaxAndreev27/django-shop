7e57a30a92ea1f07eafa0211ecea51d7a1c84462
max <maxandreev27@gmail.com>
2026-05-27 16:00:09 +0300

    Flower for monitoring celery

--- entrypoint.sh
+++ entrypoint.sh
@@ -8,5 +8,8 @@ python manage.py migrate --noinput
 echo "==> Starting Celery worker in background..."
 celery -A config worker --loglevel=info &
 
+echo "==> Starting Flower monitor on port 5555..."
+celery -A config flower --port=5555 --address=0.0.0.0 &
+
 echo "==> Starting Gunicorn server..."
-exec gunicorn --bind :8000 --workers 2 config.wsgi
+exec gunicorn --bind 0.0.0.0:8000 --workers 2 config.wsgi
