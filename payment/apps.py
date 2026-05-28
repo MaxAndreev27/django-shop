a791c6db8f2f3885f8cb2444115d443748b51ca7
max <maxandreev27@gmail.com>
2026-05-28 17:17:01 +0300

    Fix getattr

--- payment/apps.py
+++ payment/apps.py
@@ -10,9 +10,9 @@ class PaymentConfig(AppConfig):
         # print(f"Live Mode {stripe_live_mode}")
 
         # Безпечно дістаємо ключ з налаштувань
-        stripe_secret = getattr(settings, "STRIPE_SECRET_KEY", "")
+        stripe_secret = getattr(settings, "STRIPE_SECRET_KEY", "") or ""
 
-        if stripe_secret and stripe_secret.startswith("sk_test_"):
+        if stripe_secret.startswith("sk_test_"):
             print("🚀 Stripe is in TEST mode for orders. Everything is safe.")
         elif stripe_secret.startswith("sk_live_"):
             print("🚨 ATTENTION: Stripe is connected to LIVE mode in the orders app!")
